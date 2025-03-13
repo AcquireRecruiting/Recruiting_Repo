4c49d02d1e7fd1b70d12999aee81b15feac64a48
AcquireRecruiting <acquirerecruita@gmail.com>
2025-03-13 00:17:41 +0000

    new secret

--- src/ui-components/OpenAI/APIKey.js
+++ src/ui-components/OpenAI/APIKey.js
@@ -1,4 +1,4 @@
 //  Create an account of your own at https://platform.openai.com/usage
 // Get an API key of your own there
 
-export const OPENAI_API_KEY = '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';
+export const OPENAI_API_KEY = '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';
